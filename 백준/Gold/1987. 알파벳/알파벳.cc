efb9fcb47e0576701262f6c352efda45bf5dc928
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2025-05-01 19:17:50 +0900

    [Gold IV] Title: 알파벳, Time: 468 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+
+int r, c;
+vector<vector<char>> board;
+bool visited[26];
+int ans = 1;
+
+int dy[] = {-1, 1, 0, 0};
+int dx[] = {0, 0, -1, 1};
+
+void dfs(int y, int x, int acc)
+{
+    ans = max(ans, acc);
+
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny <= 0 || ny > r || nx <= 0 || nx > c) continue;
+
+        if (visited[board[ny][nx] - 'A']) continue;
+
+        visited[board[ny][nx] - 'A'] = true;
+
+        dfs(ny, nx, acc + 1);
+
+        visited[board[ny][nx] - 'A'] = false;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> r >> c;
+
+    board.assign(r + 1, vector<char>(c + 1, '\0'));
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            cin >> board[i][j];
+        }
+    }
+
+    visited[board[1][1] - 'A'] = true;
+
+    dfs(1, 1, 1);
+    cout << ans;
+
+    return 0;
+}
